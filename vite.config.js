23706f41313ee907e57f72499ff0939ada48837d
murali-kri5hna <murali.k.hemanna@fau.de>
2024-11-11 08:49:51 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // css: {
-  //   postcss: {
-  //     plugins: [
-  //       require('tailwindcss'),
-  //       require('autoprefixer'),
-  //     ],
-  //   },
-  // },
+  base: '/',  // Change this to '/' since we're deploying to murali-kri5hna.github.io
+  build: {
+    outDir: 'dist',
+  },
 })
